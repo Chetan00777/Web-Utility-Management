e0f3afefd180ea9943979f0a2a78c2699ef089db
EC2 Default User <ec2-user@ip-172-31-0-33.ec2.internal>
2025-11-16 14:46:49 +0000

    Enable Whitenoise for staticfiles on EB

--- utility_management/settings.py
+++ utility_management/settings.py
@@ -28,6 +28,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'utility_management.urls'
